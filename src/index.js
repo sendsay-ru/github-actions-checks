6071de8102258a3b5d5e9e1f82ef23f9b073fc0e
Aleksey Nikolaev <nikolaev@iprojects.ru>
2023-08-06 14:06:53 +0300

    feat(github-action-checks): init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const source = require('../config/source.json');
+const { render, getConfig, checkDirs, saveFiles } = require('./utils');
+
+checkDirs();
+
+const config = getConfig();
+
+const compiled = render(source, config);
+saveFiles(compiled);
