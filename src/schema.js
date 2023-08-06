a9c85611c34927d413e4d32a402208a3bee7d6fe
Aleksey Nikolaev <nikolaev@iprojects.ru>
2023-08-06 14:06:53 +0300

    feat(github-action-checks): init

---
+++ src/schema.js
@@ -0,0 +1,23 @@
+const Joi = require('joi');
+
+module.exports = Joi.object({
+  jobs: Joi.array()
+    .required()
+    .items(
+      Joi.object({
+        key: Joi.string().required(),
+        steps: Joi.array()
+          .required()
+          .items(
+            Joi.object({
+              key: Joi.string().required(),
+              name: Joi.string().required(),
+              command: Joi.string().required(),
+            }),
+          ),
+      }),
+    ),
+  node: Joi.any(),
+  installCommand: Joi.string().empty(''),
+  buildCommand: Joi.string().empty(''),
+});
